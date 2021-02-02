06e91fe0001d2e65e291128cd1f7c4ce9ff46266
Michael Lai <mrlai7782@gmail.com>
2021-02-02 01:50:12 -0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* const person = [
+    {
+        name: "bob",
+        age: 25,
+        todo: [
+            "walk",
+            "eat",
+            "talk"
+        ]
+    },
+    {
+        name: "tom",
+        age: 25,
+        todo: [
+            "walk",
+            "eat",
+            "talk"
+        ]
+    }
+];
+
+console.log(person);
+
+console.log(JSON.stringify(person));
+ */
+
+ 
