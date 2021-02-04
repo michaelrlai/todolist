b9af0709c633b8ecbefb8798761bd81b5390aeaf
Michael Lai <mrlai7782@gmail.com>
2021-02-03 18:55:58 -0800

    Added buttons

--- src/display.js
+++ src/display.js
@@ -8,7 +8,22 @@ const displayHeader = function() { // Create and display header
     const header = document.createElement("div");
     header.classList.add("header");
     selectPage().appendChild(header);
-    return header;
+
+    const headerButtonContainer = document.createElement("div");
+    headerButtonContainer.classList.add("header-button-container");
+    header.appendChild(headerButtonContainer);
+
+    const buttons = ["+"];
+    buttons.forEach(element => {
+        const button = document.createElement("div");
+        button.classList.add("button");
+        button.classList.add("header-button");
+        button.textContent = element;
+        headerButtonContainer.appendChild(button);
+    });
+
+
+    console.log("hi");
 };
 
 const createMain = function() { // Create main area below header, includes projects and todo lists
@@ -31,6 +46,7 @@ const displayProjects = function() {
         const buttons = ["Inbox", "Projects"];
         buttons.forEach(element => {
             const button = document.createElement("div");
+            button.classList.add("button");
             button.classList.add("projects-button");
             button.classList.add(`button-${element.toLowerCase()}`);
             button.textContent = element;
